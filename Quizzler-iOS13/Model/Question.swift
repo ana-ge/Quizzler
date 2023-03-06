3518e3c1098d5c5c2445e5cd964c08a3e0db974d
Ge Anatoliy <117572434+ana-ge@users.noreply.github.com>
2023-03-06 22:44:43 +0900

    Update Question.swift

--- Quizzler-iOS13/Model/Question.swift
+++ Quizzler-iOS13/Model/Question.swift
@@ -1,11 +1,3 @@
-//
-//  Question.swift
-//  Quizzler-iOS13
-//
-//  Created by Анатолий Ге on 06.02.2023.
-//  Copyright © 2023 The App Brewery. All rights reserved.
-//
-
 import Foundation
 
 struct Question {
